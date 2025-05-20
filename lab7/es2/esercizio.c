59a04bf00e57077aa18bcb455f82a95eed6d60bc
caradav <davidecaramagna5@gmail.com>
2025-05-20 16:09:07 +0200

    sinch

--- lab7/es2/esercizio.c
+++ lab7/es2/esercizio.c
@@ -6,4 +6,30 @@ typedef struct Exercise{
   float val;
 } Exercise;
 
-struct Exercise *createExercise
+struct Exercise **readFile(FILE *infile, struct Exercise **e, int lenFile){
+  *e = malloc(lenFile*sizeof(struct Exercise));
+
+  char name[100];
+  for(int i=0; i<lenFile; i++){
+    fscanf(infile, "%s %d %d %d %d %d %f %d", name, &e[i]->type, &e[i]->entry, &e[i]->out, &e[i]->prec, &e[i]->final, &e[i]->val, &e[i]->diff);
+    strcpy(e[i]->name, name);
+  }
+
+  return e;
+}
+
+void printExercise(struct Exercise *e){
+  printf("%s %d %d %d %d %d %f %d\n", e->name, e->type, e->entry, e->out, e->prec, e->final, e->val, e->diff);
+}
+
+int getType(struct Exercise *e){
+  return e->type;
+}
+
+int getEntry(struct Exercise *e){
+  return e->entry;
+}
+
+int getOut(struct Exercise *e){
+  return e->out;
+}
--- lab7/es2/esercizio.h
+++ lab7/es2/esercizio.h
@@ -1,6 +1,9 @@
 #ifndef ESERCIZIO_H
 #define ESERCIZIO_H
 
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
 
 
 #endif
